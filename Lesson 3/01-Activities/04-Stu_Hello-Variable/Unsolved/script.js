aed5d45141a54672ba53cd6fdecc7f901d847d2b
Nevin Lewis <lewisnevin@utexas.edu>
2022-09-21 12:48:08 -0500

    class lesson notes

--- Lesson 3/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ Lesson 3/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,7 @@
 // Write Your JavaScript Code Here
+var personName = "Nevin Lewis"
+var pets = "1"
+var funfacts = "I biked from Austin to Alaska"
+console.log(`My name is ${personName}` )
+console.log(`I have ${pets} pet`)
+console.log("Fun fact is:" + funfacts)
